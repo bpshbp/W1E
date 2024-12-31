57f5f6b2e144c5f7f035924ef2b664c3374fb06f
www.Web1Expert.com <besumitbe@gmail.com>
2024-12-31 09:22:08 +0530

    Create Example.php

---
+++ Example.php
@@ -0,0 +1,228 @@
+Which plan offer seperate cpanel in shared,vps,cloud ...actually i want to start resell business ..whats pricing....any discount.....if something happen to me how can my client recover than account
+I have sent mail besumitbe@gmail.com  for setting my affiliate account can i know the status
+getting this Duplicate Google how to chose different canonical than user for https://web1expert.com/
+
+Domain
+is Free domain WHOIS privacy protection for domain transferred to hostinger too & is it free for life for all domain transferred & are there discounts if i transferred my all domains
+ERROR Report domain: share-ask.com Submitter: google.com https://www.awesomescreenshot.com/image/52056923?key=fb03678467e5425f89e482c3b361fc1b
+TRANSFER DOMAIN TO HOSTINGER OR CHEAP PROVIDER WITH FREE PRIVACY LIFETIME LIKE NAMECHEAP
+What r Parked Domains is it free REDIRECT
+What r Google Workspace plan business offered by you plz mail me
+This is my clients site https://centrallawagency.com/ will he be able to recover it 
+
+
+HOW CAN I SAVE SERVER SPACE, Bandwith AS ITS GOING TO BE FULL SOON
+
+PLUGIN
+Plugin disable right click on html & wordpress Plugin to disable or unlink all external link
+PLUGIN TO SAVE SERVER SPACE,Bandwith
+LITESPEEDCACHE OR WP-OPTIMISE
+Getting error here https://web1expert.com/blog/top-webhosting-companys/ where images are not displaying
+ADD Baidu YANDEX GWT
+NEED DISABLE RIGHT CLICK CODE for wp & html site
+Should i Convert to InnoDB for wp_posts in WP-Optimize PLUGIN
+Yoast schema or plugin in WordPress
+INSTALL WP Last Modified Info CHILD THEME FOR ALL
+how to reset litespeed cache setting is there any plugin who do reset any plugin to original default setting
+
+
+
+DATABASE
+MYSQL QUERY FOR WEBSITE SPEED OPTIMISATION
+1 DATABASE FOR MULTIPLE SITE WILL IT SOLVE SERVER SPACE ISSUE OF MULTIPLE DATABASE IN WORDPRESS IF NO IS THERE ANY OPTION
+
+CODING
+need code to open any page in new window
+What is WP-CLI and SSH ,Secure access manager,Free 1-click Google integration & its uses & how to use it
+This https://web1expert.com/#testimonial is not working
+What BEST PERMISSION FOR files,FOLDERS IN WORDPRESS,HTML,ETC WEBSITE FOR SECURITY from hacking plz
+tmp files r they necessary to get server space if no then what should i clear more
+do you find heavy files on my server like zip etc whih r not related which i can use to get space
+my all updraft backup r going to files instead of googledrive can u plz check how to stop
+updraft or hostinger 
+WP HTML Sitemap Page without plugin php code
+
+hpanel error https://www.awesomescreenshot.com/image/52055032?key=6a889d1cb3c6bdebef8942edca55f54d
+https://www.awesomescreenshot.com/image/52057447?key=8806b30586efa10f8136b27c1b69c1fe
+
+W1E domain transfer fee compare
+Why my all domain under this account bpshbp@gmail.com after activating child theme are looking different they should look like this theme demo also my html which r installed in root here fr web1expert.com https://cheapwebhostpro.com/ r showing boxed width 
+I want like that  https://web1expert.com/blog/get-startup-website-absolutely-free/ where all link goes to new path https://web1expert.com/blog/ but i dont want  manual redirection & i dont want my https://web1expert.com/index.html or https://web1expert.com or redirected
+i mean all domain url should go to domain/blog without redirection manually
+https://cheapwebhostpro.com/ IS showing boxed width instead of full width...i need fullwidth page like this demo https://htmlcodex.com/demo/?item=2070
+how to comment out entire <body> or <head> or <html> tag code
+
+How to REMOVE UNUSED PLUGIN ENTIRELY from database
+OBJECT OR LITE SPEED CACHE OR WP-OPTIMISE WHICH IS BEST
+Y is my site share-ask.com ,web1expert.com,CheapWebHostPro.com,MyBlogInspiration.com r slow
+can u plz check in my hpanel which is extra database not linked to share-ask.com ,web1expert.com,CheapWebHostPro.com,MyBlogInspiration.com & remove it plz
+http://64.91.241.209/web1expert.tar.gz
+should i redirect if url is changed & giving 404 error
+Make custom 404 Page
+
+
+
+
+
+SEO
+FIX GOOGLE WEBMASTER TOOL ERROR https://www.awesomescreenshot.com/image/52112457?key=27d2bc86537b900ef8c1f67a4095ab28
+HTTPS page has internal links to HTTP New	15	+15	
+Orphan page (has no incoming internal links) New
+or 0.5$ per 1000words article ,so its manadatory Will be increased as site grows &  if your performance increase thats guarantee
+plz fix 404. That’s an error. https://www.google.com/sitemaps/ping?sitemap=https://CheapWebHostPro.com/blog/sitemap_index.xml
+FIX AUTO AD OR MANUSAL AD
+SHOULD U BLOCK AI
+
+HOSTINGER
+whats Remote MySQL
+
+
+which IP Address should i block to save from hackers
+HPANEL ERROR https://www.awesomescreenshot.com/image/52182979 not able to change php version https/cheapwebhostpro.com
+Can i get website backup on my gmail when requested plz 
+can u BLOCK recent attacks IP
+I't to add 2 gmail in 1 hostinger a/c
+How to use How to Use HOSTINGER AI Assistant? https://support.hostinger.com/en/articles/8238149-how-to-use-the-wordpress-ai-assistant
+what is cpanel ,vps,cloud,dedicted hosting...where do i get seperte cpanel'....can i sell ur plans on my site which is cheapest plan without buying or if  i buy it & sell it ...what do u suggest i should do for cheapest client
+
+ERROR for my all domain Warning, your server signature is ON.
+
+
+
+
+SERVER
+SERVER SAVE SPACE
+how to or trick to save bandwidth,server space,file space,inode space 
+IVE HEAVY wordpress FOLDER IS THERE WAY TO UPLOAD HEAVY FOLDER to EXTERNAL SITE to SERVER SAVE SPACE 
+plugin to trace heavy plugins Query Monitor 
+is it ok to host image folder externally if you have cloudflare integrated already 
+plugin to host image,database,html files & folder externally if you have cloudflare integrated already 
+
+how to stop my all unnecessary html pages no index from google search
+check https://sitecheck.sucuri.net/results/https/cheapwebhostpro.com it shows no firewall 
+No website application firewall detected. Please install a cloud-based WAF to prevent website hacks and DDoS attacks for all my site in my account asap
+whats SSH & y should i use it 
+whats Command line & y should i use it 
+whats git & y should i use it 
+CLOUDFLARE ERROR https://www.awesomescreenshot.com/image/51698420?key=79fddd46c7451c0dd27dc4d2b2f792a5
+is ASIA server good for world clients
+I got Hostinger 50% of resources used in the last 24 hours message how do i fix that without upgrading ,can you plz guide to save bandwidth,server space,file space,inode space as i really dont have money now
+Plz check my sites in my account & suggest ho do i optimise html,wordpress website as i dont have budget
+is it necessary to Password Protect Directories & which directories should i Password Protect in html,css,wordpress website
+My all domains in my a/c bpshbp@gmail.com r not getting cached this tool says https://www.duplichecker.com/google-cache-checker.php
+
+
+
+Wordpress BUG
+which Directories should i Password Protect for wordpress security
+plz fix wordpress invalid installation error in hpanel https://www.awesomescreenshot.com/image/52057447?key=8806b30586efa10f8136b27c1b69c1fe
+plz fix https://share-ask.com/ https://share-ask.com/ is still down.Reason: HTTP error 500.
+how to configure webmail with android
+https://web1expert.com/blog/wp-login.php m unable to login without any error https://www.awesomescreenshot.com/image/52183752?key=a2006d3641f99d6646aa2bcbabb4a4dd
+plz fix broken https://cheapwebhostpro.com/blog/
+wordfence plugin if i enable it creates this login issue  without any error https://www.awesomescreenshot.com/image/52183752?key=a2006d3641f99d6646aa2bcbabb4a4dd
+
+Wordpress optimisation
+Plz tell me how do i remove ununsed tables from my database using plugin or sql queries & safely save database space
+How do i do below
+Using indexes on frequently queried columns
+Optimizing queries to retrieve only the necessary data
+Avoiding duplicate data
+Applying regular database maintenance
+Best code for wp-congig.php,robots.txt
+AUTOOPTIMISER PLUGIN SETUP 
+yoast schema or another plugin
+
+Wordpress PLUGIN
+WP_OPTIMISE OR SMUSH OR LITESPEEDCACHE
+
+BUG https://web1expert.com/#testimonial
+ive 2 remove redirect from my pc chrome...how to do via editing host file...https://web1expert.com/wp-admin/ redirect to http://127.0.0.1/ https://www.awesomescreenshot.com/video/34679864
+Error: Cookies are blocked or not supported by your browser. You must enable cookies to use WordPress.
+whats Cron Jobs & y should i use it ....i need it to set auto clear my cache
+should i create seperate sitemap for html & wordpress
+whats Folder Index Manager & y should i use it & which folder should i index in html,css,wordpress website  & which r best practises
+whats INNODES & y should i use it & which r best practises
+Plz disable automatic Wordpress core update for all sites asap
+ASK NATHAN UPDRAFT OR HOSTINGER BU
+generatepress child theme
+FIX REDIRECT /BLOG
+ADD ALL CV OR COMMON WEBSITE CV https://web1expert.com/blog/expert/
+REMOVE ALL CAPTION
+EDIT https://web1expert.com/blog/gallery/
+DATABASE CONNECTION GET LOST WHEN I EDIT ANYTHING ON https://web1expert.com & also https://web1expert.com/blog/wp-admin is very slow
+GENERATEPRESS FULLWIDTH LAYOUT
+IMAGES R NOT UPLOADING HERE https://web1expert.com ERROR cropped-cropped-Sunlight-Yoga.png
+The server cannot process the image. This can happen if the server is busy or does not have enough resources to complete the task. Uploading a smaller image may help. Suggested maximum size is 2560 pixels.
+
+ADSENSE
+TURNOFF URL
+INSTALL Auto Affiliate Links https://wordpress.org/plugins/wp-auto-affiliate-links/
+
+
+
+
+https://www.milesweb.in/hosting/cloud-hosting/cheap-cloud-hosting
+https://www.milesweb.in/hosting/web-hosting
+https://www.liquidweb.com/blog/cheapest-vps-hosting/
+
+
+
+SEO
+EXTERNAL LINK Share-Ask.com,MyBlogInspiration.com,Web1Expert.com,CheapWebHostPro.com
+Sitemap couldnt fetch error
+add notification plugin
+Schema & Structured Data for WP & AMP setup in CWP
+ALWAYS ADD one external link per page
+orphan page link plugin
+ADD FREE TOOLS ON WEBSITE FREE Heatmaps TOOL
+
+
+HPANEL DBASE RESTORE PROCESS
+hPanel >> Backup >> Database Backup >> Choose DB (use this one : u818595528_IsCfd)
+And Choose the date to restore :
+
+SELL SERVER
+i need cheapest  unlimited cpanel  & unlimited  website plan for selling to my cheapest client in nigeria plz suggest
+
+
+
+
+Hostinger	$1.99/month	Free SSL, 100 GB bandwidth, 1 website, 30 GB SSD storage.	Great for small websites.
+IONOS	$2.00/month	Unlimited bandwidth, free domain for 1 year, 10 GB SSD storage.	Best value for bandwidth.
+Namecheap	$1.88/month	Free domain, 20 GB SSD storage, unmetered bandwidth.	Best for beginners.
+Best Choice: Namecheap is slightly cheaper and includes a free domain, making it ideal for startups or small sites.
+
+VPS Hosting
+Provider	Starting Price	Features	Verdict
+Hostinger	$2.99/month	1 vCPU, 20 GB SSD storage, 1 GB RAM, 1 TB bandwidth.	Balanced performance.
+IONOS	$2.00/month	1 vCPU, 10 GB SSD storage, 512 MB RAM, unlimited bandwidth.	Cheapest with bandwidth.
+SurferCloud	$1.00/month	Customizable resources, pay-as-you-go pricing.	Cheapest overall.
+Best Choice: Hostinger offers better specs for the price. SurferCloud is good for ultra-budget needs but may lack features.
+
+Cloud Hosting
+Provider	Starting Price	Features	Verdict
+Kamatera	$4.00/month	Pay-as-you-go, scalable, fully customizable server.	Flexible for growth.
+Hostinger	$9.99/month	3 vCPU, 6 GB RAM, 200 GB SSD storage.	Best performance.
+IONOS	$5.00/month	Unlimited bandwidth, flexible scaling, SSD storage.	Cheapest overall.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
